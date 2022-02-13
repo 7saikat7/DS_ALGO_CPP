8cac316855ff5bc42b1a77afe6af7769b598cc76
Saikat Mukherjee <65228695+7saikat7@users.noreply.github.com>
2022-02-13 19:58:40 +0530

    Time: 46 ms (37.86%), Space: 23.3 MB (12.45%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -14,7 +14,7 @@ public:
     if(root==NULL || root==p ||root==q) return root;
     TreeNode* left=lowestCommonAncestor(root->left,p,q);
     TreeNode* right=lowestCommonAncestor(root->right,p,q);
-     
+                                                        
     if(left==NULL) return right;
     else if(right==NULL) return left;
         
