a1695e38f903978f3d327b5fde9a54c07cf83e4e
Saikat Mukherjee <65228695+7saikat7@users.noreply.github.com>
2022-03-11 11:31:57 +0530

    Time: 11 ms (89.63%), Space: 12.5 MB (73.94%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    struct comp_func{
+
+        bool operator()(const pair<string,int> &a,const pair<string,int> &b){
+            return a.second>b.second || (a.second==b.second && a.first<b.first);
+    }
+    };
+public:
+   
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string,int> mpp;
+        vector<string> ans;
+        for(auto &it:words){
+            mpp[it]++;
+        }
+        priority_queue<pair<string,int>,vector<pair<string,int>>,comp_func> minheap;
+        
+        for(const auto &it:mpp){
+            if(minheap.size()<k){
+                minheap.push({it.first,it.second});
+            }
+            else{
+                if(it.second>minheap.top().second){
+                    minheap.pop();
+                    minheap.push({it.first,it.second});
+                }
+                else if(it.second==minheap.top().second){
+                    
+                    if(it.first<minheap.top().first){
+                        minheap.pop();
+                        minheap.push({it.first,it.second});}
+                }
+            }
+        }
+        while(!minheap.empty()){
+            ans.push_back(minheap.top().first);
+            minheap.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
