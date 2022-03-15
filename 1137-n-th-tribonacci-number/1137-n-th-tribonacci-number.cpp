f0b5337fe043ba0f0977aad674880387e1449df5
Saikat Mukherjee <65228695+7saikat7@users.noreply.github.com>
2022-03-16 01:03:46 +0530

    Time: 2 ms (32.07%), Space: 5.9 MB (83.21%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+     if(n<2) return n;
+     if(n==2) return 1;
+     int ans[n+1];
+     ans[0]=0;ans[1]=1;ans[2]=1;
+         for(int i=3;i<=n;i++){
+            ans[i]=ans[i-1]+ans[i-2]+ans[i-3];
+         }
+     return ans[n];
+    }
+};
