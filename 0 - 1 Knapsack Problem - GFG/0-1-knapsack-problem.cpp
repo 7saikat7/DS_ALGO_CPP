889d440f4e2bcfddd16a107d78b28663ad7a750d
Saikat Mukherjee <65228695+7saikat7@users.noreply.github.com>
2022-04-08 00:24:22 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -12,11 +12,9 @@ class Solution
     { 
        int t[n+1][W+1];
        
-       for(int i=0; i<=n; i++)
-       t[i][0]=0;
-       
-       for(int j=0; j<=W; j++)
-       t[0][j]=0;
+       for(int i=0; i<=n; i++){ 
+       for(int j=0; j<=W; j++){
+       if(i==0 || j==0) t[i][j]=0;}}
        
        for(int i=1;i<n+1;i++){
            for(int j=1;j<W+1;j++){
