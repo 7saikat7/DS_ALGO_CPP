dcd5ab89a562d27891843a8738b589bcc013f980
Saikat Mukherjee <65228695+7saikat7@users.noreply.github.com>
2022-04-17 15:56:21 +0530

    Time: 6 ms (20.69%), Space: 8.1 MB (19.70%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
@@ -10,20 +10,20 @@
  * };
  */
 class Solution {
+public:
+    void maketree(TreeNode* &ans,TreeNode* root){
+         if(!root) return;
+         maketree(ans,root->left);
+         ans->right=new TreeNode(root->val);
+         ans=ans->right;
+         maketree(ans,root->right);
+     }
+     TreeNode* increasingBST(TreeNode* root) {
+     TreeNode* temp;
+     TreeNode* ans=new TreeNode(0);
+     temp=ans;
+     maketree(ans,root);
+     return temp->right; 
+    }
     
-    public:
-  void inorder(TreeNode*& ans, TreeNode* root) {
-        if (!root) return;
-        inorder(ans, root->left);
-        ans->right = new TreeNode(root->val);
-        ans = ans->right;
-        inorder(ans, root->right);
-    }
-    TreeNode* increasingBST(TreeNode* root) {
-        TreeNode* temp;
-        TreeNode* ans = new TreeNode();
-        temp = ans;
-        inorder(ans, root);
-        return temp->right;
-    }
 };
