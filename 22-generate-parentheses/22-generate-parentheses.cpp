eaf8b903cfcf990b759c6847e3664c67e0f28a89
Saikat Mukherjee <65228695+7saikat7@users.noreply.github.com>
2022-05-03 12:26:58 +0530

    Time: 15 ms (13.43%), Space: 15.7 MB (19.34%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,16 +1,43 @@
 class Solution {
 public:
-   vector<string> generateParenthesis(int n) {
-        vector<string> res;
-        addingpar(res, "", n, 0);
-        return res;
-    }
-    void addingpar(vector<string> &v, string str, int n, int m){
-        if(n==0 && m==0) {
-            v.push_back(str);
+    void find(int st,int end,string temp,vector<string> &ans)
+    {
+        cout<<temp<<endl;
+        if(st==0 && end==0){
+            ans.push_back(temp);
             return;
         }
-        if(m > 0){ addingpar(v, str+")", n, m-1); }
-        if(n > 0){ addingpar(v, str+"(", n-1, m+1); }
+        if(st==end)
+        {
+            
+            temp+='(';
+            find(st-1,end,temp,ans);
+            temp.pop_back();
+        }
+        else{
+        if(st>0 && st<end)
+        {
+             
+            temp+='(';
+            find(st-1,end,temp,ans);
+           temp.pop_back();
+        }
+        if(end>0 && end>st)
+        {
+            
+            temp+=')';
+            find(st,end-1,temp,ans);
+            
+        }
+        }
+        
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string str="";
+        int st=n;
+        int end=n;
+        find(st,end,str,ans);
+        return ans;
     }
 };
